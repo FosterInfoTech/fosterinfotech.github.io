f35df234328cc363eb84d204e9a10c0a931501b8
bronzr <46513893+bronzr@users.noreply.github.com>
2021-07-13 19:52:29 -0500

    Add files via upload

---
+++ files/plugins.js
@@ -0,0 +1,37 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery(document).ready(function(a){function b(){a("#about .skill").length&&a("#about .skill").each(function(){var b=a(this),c=a(b).children(".skill-top").children(".skill-progress-bar"),d=a(b).data("skill-progress-bar-width"),e=a(b).data("skill-color");a(b).children(".skill-bottom");a(c).progressbar({value:d}),a(this).children(".skill-top").children(".skill-progress-bar").children(".ui-progressbar-value").css("background-color",e),a(this).children(".skill-top").children(".skill-progress-bar").children(".ui-progressbar-value").append('<span class="ui-progressbar-value-circle" style="background-color: '+e+'"></span>'),a(this).children(".skill-top").children(".skill-progress-bar").children(".ui-progressbar-value").append('<span class="ui-progressbar-value-top" style="background-color: '+e+'"></span>'),a(this).children(".skill-top").children(".skill-progress-bar").children(".ui-progressbar-value").children(".ui-progressbar-value-top").text(d+"%"),a(this).children(".skill-top").children(".skill-progress-bar").children(".ui-progressbar-value").children(".ui-progressbar-value-top").append('<span class="ui-progressbar-value-triangle" style="border-top-color: '+e+'; border-right-color: transparent; border-bottom-color: transparent; border-left-color: transparent;"></span>'),a(this).children(".skill-bottom").css("color",e)})}function c(){a(".testimonials-carousel.owl-carousel-enabled").length&&a(".testimonials-carousel.owl-carousel-enabled").owlCarousel({items:1,loop:!0,dots:!0})}function d(){a("#counter .counter-number").length&&a("#counter .counter-number").countTo()}a(function(){b(),c(),a(window).scroll(function(){var b=a("#counter").visible();1==b&&d()})})});
+//# sourceMappingURL=plugins.min.js.map
+
+}
+/*
+     FILE ARCHIVED ON 06:24:57 Dec 21, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 00:10:44 Jul 14, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 93.976
+  exclusion.robots: 0.135
+  exclusion.robots.policy: 0.124
+  RedisCDXSource: 2.38
+  esindex: 0.01
+  LoadShardBlock: 66.423 (3)
+  PetaboxLoader3.datanode: 836.647 (5)
+  CDXLines.iter: 22.267 (3)
+  load_resource: 1206.224 (2)
+  PetaboxLoader3.resolve: 388.923 (2)
+*/
